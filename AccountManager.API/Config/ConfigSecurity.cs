6db7106eb97de00bbeea0c480e5792b41001aa4b
jrdivison <j.divison@deadlocksolutions.com>
2019-06-01 10:24:48 -0400

    Add JWT Secret

---
+++ AccountManager.API/Config/ConfigSecurity.cs
@@ -0,0 +1,21 @@
+﻿namespace AccountManager.API.Config
+{
+    using AccountManager.API.Security;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using System.Text;
+
+    public static class ConfigSecurity
+    {
+        public static IServiceCollection ConfigureService(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<SecurityDbContext>();
+
+            var key = Encoding.ASCII.GetBytes(configuration["JWT:secret"]);
+            return services;
+        }
+    }
+}
---
+++ AccountManager.API/Security/SecurityDbDesignTimeFactory.cs
@@ -0,0 +1,26 @@
+﻿using AccountManager.API.Security;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountManager.Data.Factory
+{
+    public class SecurityDbDesignTimeFactory
+        : IDesignTimeDbContextFactory<SecurityDbContext>
+    {
+        public SecurityDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = 
+                new DbContextOptionsBuilder<SecurityDbContext>();
+
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SecurityDB;Integrated Security=True",
+                option=> option.MigrationsAssembly(
+                    typeof(SecurityDbContext).Assembly.FullName));
+
+            var ctx = new SecurityDbContext(optionsBuilder.Options);
+                 return ctx;
+        }
+    }
+}
